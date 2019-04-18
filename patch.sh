619075745b5fb6d1253ca21b1bfdcb5676b819e5
CHMEX <33424502+CHMEX@users.noreply.github.com>
2019-04-18 07:20:49 +0200

    Create patch.sh
    how to use it?
    chmod +x patch.sh
    ./patch.sh CHAINNAME #example patch.sh DEX

---
+++ patch.sh
@@ -0,0 +1,7 @@
+
+
+cp status.html ~/explorer/$1-explorer/node_modules/insight-ui-komodo/public/views/status.html
+cp header.html ~/explorer/$1-explorer/node_modules/insight-ui-komodo/public/views/includes/header.html
+cp status.js ~/explorer/$1-explorer/node_modules/insight-api-komodo/lib/status.js
+cp bitcoind.js ~/explorer/$1-explorer/node_modules/bitcore-node-komodo/lib/services/bitcoind.js
+
